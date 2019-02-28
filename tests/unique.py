0736f41a808429010fe7404c3becb206d024540e
Irene Martínez <iree802@gmail.com>
2019-02-28 11:12:08 +0100

    Update tests/unique.py

---
+++ tests/unique.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+def main():
+    i = np.array([0, 1, 2])
+    df = ( pd.DataFrame(np.array([ ['A', 'x', '3'], 
+                                   ['B', 'y', '4'], 
+                                   ['A', 'x', '5'],
+                                   ['B', 'x', '6'] ]),
+                             columns=['domain', 'ip', 'time']) )
+    result = ( pd.DataFrame(np.array([ ['A', 'x'], 
+                                       ['B', 'y'],
+                                       ['B', 'x'] ]),
+                             columns=['domain', 'ip']) )
+    df = unique_tuples(df)
+    print(df)
+
+def unique_tuples(df):
+    unique_df = df.drop_duplicates(subset=('domain', 'ip')).drop(labels = 'time',axis = 1)
+    return unique_df
+
+main()
